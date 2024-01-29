c16ac5214b9654231bbdae4d0a7b6907edd0d097
Fullofb3ans <igorgazinskii6@gmail.com>
2024-01-29 14:08:35 +0400

    patch

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,66 +3,57 @@ import { Cards } from '../components/cards';
 import { Preloader } from '../components/preloader';
 import { Search } from '../components/Search';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = '1673431';
 
 class Main extends React.Component {
-  state = {
-    cards: [],
-    filter: '',
-    loading: false,
-  };
+	state = {
+		cards: [],
+		filter: '',
+		loading: false,
+	};
 
-  componentDidMount() {
-    this.setState({ loading: true });
-    fetch(`https://www.omdbapi.com/?s=evangelion&apikey=${API_KEY}`)
-      .then((response) => response.json())
-      .then((data) => this.setState({ cards: data.Search, loading: false }))
-      .catch((err) => {
-        console.error(err);
-        this.setState({ loading: false });
-      });
-  }
+	componentDidMount() {
+		this.setState({ loading: true });
+		fetch(`https://www.omdbapi.com/?s=evangelion&apikey=${API_KEY}`)
+			.then((response) => response.json())
+			.then((data) => this.setState({ cards: data.Search, loading: false }))
+			.catch((err) => {
+				console.error(err);
+				this.setState({ loading: false });
+			});
+	}
 
-  searchUpdate = (search) => {
-    this.setState({ cards: [], loading: true });
-    try {
-      fetch(
-        `https://www.omdbapi.com/?s=${search}&type=${this.state.filter}&apikey=${API_KEY}`
-      )
-        .then((response) => response.json())
-        .then((data) =>
-          data.Response === 'True'
-            ? this.setState({ cards: data.Search, loading: false })
-            : this.setState({ cards: [], loading: false })
-        );
-    } catch (error) {
-      console.log('error');
-    }
-  };
+	searchUpdate = (search) => {
+		this.setState({ cards: [], loading: true });
+		try {
+			fetch(`https://www.omdbapi.com/?s=${search}&type=${this.state.filter}&apikey=${API_KEY}`)
+				.then((response) => response.json())
+				.then((data) => (data.Response === 'True' ? this.setState({ cards: data.Search, loading: false }) : this.setState({ cards: [], loading: false })));
+		} catch (error) {
+			console.log('error');
+		}
+	};
 
-  filterByRadio = (str) => {
-    this.setState({ filter: str });
-  };
+	filterByRadio = (str) => {
+		this.setState({ filter: str });
+	};
 
-  render() {
-    return (
-      <main>
-        <Search su={this.searchUpdate} fbr={this.filterByRadio} />
-        {this.state.cards.length === 0 && this.state.loading === false ? (
-          <div className='empty'>
-            <h3>Nothing found</h3>
-          </div>
-        ) : (
-          <h1></h1>
-        )}
-        {this.state.loading === true ? (
-          <div className='placeForPreloader'>{<Preloader />}</div>
-        ) : (
-          <Cards cards={this.state.cards} />
-        )}
-      </main>
-    );
-  }
+	render() {
+		console.log(this.state.cards);
+		return (
+			<main>
+				<Search su={this.searchUpdate} fbr={this.filterByRadio} />
+				{this.state.cards.length === 0 && this.state.loading === false ? (
+					<div className="empty">
+						<h3>Nothing found</h3>
+					</div>
+				) : (
+					<h1></h1>
+				)}
+				{this.state.loading === true ? <div className="placeForPreloader">{<Preloader />}</div> : <Cards cards={this.state.cards} />}
+			</main>
+		);
+	}
 }
 
 export { Main };
